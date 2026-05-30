475840ed62f00e8f5c148df19d690f8f4cf7a930
Альберт <albertdrynkin600@gmail.com>
2026-05-30 19:19:52 +0300

    don't work count new good main

--- app/main.py
+++ app/main.py
@@ -1,12 +1,9 @@
-import os
 import asyncio
-from aiogram import Bot, Dispatcher
-from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_application
-from aiohttp import web
 from dotenv import load_dotenv
 
 load_dotenv()
 
+from aiogram import Bot, Dispatcher
 from app.core.config import settings
 from app.core.database import SessionLocal, init_db
 from app.midllewares.db import DbSessionMiddleware
@@ -26,39 +23,26 @@ async def on_startup_notify(bot: Bot):
             print(f"Не удалось отправить пользователю {tg_id}: {e}")
     print("Рассылка завершена успешно!")
 
-async def on_startup_polling(bot: Bot):
+async def main(should_notify: bool):
     await init_db()
-    asyncio.create_task(on_startup_notify(bot))
-    print("Бот запущен через Polling (Локально)!")
-
-async def on_startup_webhook(bot: Bot):
-    await init_db()
-    webhook_url = os.getenv("RENDER_EXTERNAL_URL")
-    await bot.set_webhook(url=f"{webhook_url}/webhook")
-    asyncio.create_task(on_startup_notify(bot))
-    print(f"Бот запущен через Webhook на сервере: {webhook_url}/webhook")
-
-def main():
+    
     bot = Bot(token=settings.bot_token)
     dp = Dispatcher()
+    
+    if should_notify:
+        dp.startup.register(on_startup_notify)
+
     dp.update.middleware(DbSessionMiddleware())
     dp.include_router(router)
 
-    if os.getenv("RENDER_EXTERNAL_URL"):
-        dp.startup.register(on_startup_webhook)
-        
-        app = web.Application()
-        SimpleRequestHandler(dispatcher=dp, bot=bot).register(app, path="/webhook")
-        setup_application(app, dp, bot=bot)
-        
-        port = int(os.getenv("PORT", 8080))
-        print("Запуск веб-сервера для Render...")
-        web.run_app(app, host="0.0.0.0", port=port)
+    print("Bot started via Polling (Locally)!")
     
-    else:
-        dp.startup.register(on_startup_polling)
-        print("Запуск обычного Polling...")
-        asyncio.run(dp.start_polling(bot))
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
 
 if __name__ == "__main__":
-    main()
+    user_input = input("Введите 'да', чтобы начать рассылку, или нажмите Enter, чтобы пропустить: ").strip().lower()
+    run_notification = (user_input == 'да')
+    
+    # Запускаем бота и передаем сохраненный флаг
+    asyncio.run(main(should_notify=run_notification))
